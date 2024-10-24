494ab98088b8ae21f181b3338e59b7c5a8759e1f
Marcos-System <silvamarcos6287@gmail.com>
2024-10-24 16:08:24 +0000

    teste

--- administra/settings.py
+++ administra/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wli27&m10w)-ft%!nmv9$)dx#fo-2-2)z&-n*yfl^s)+l4rb1#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marcosborges.pythonanywhere.com']
 
 
 # Application definition
